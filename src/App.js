f5d5ca7f49817adefedbc77fd8ad88471457fc14
John Paulin <genmansystems@gmail.com>
2022-07-25 01:21:06 +0800

    tresting

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Testing continuous deployment.
         </a>
       </header>
     </div>
